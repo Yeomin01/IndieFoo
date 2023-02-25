027d628996a1e6222770529fae9b2c687b346ea3
Yeomin <kam3546@gmail.com>
2023-02-25 22:28:14 +0900

    [contact] nav+url+ani

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		// header.style.backgroundColor = ' rgba(0,0,0, 0.3)';
+		header.style.backgroundColor = ' rgba(255,255,255, 0.5)';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
@@ -22,6 +22,7 @@ menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
+    menu_item.classList.toggle('active');
 	});
 });
 
@@ -47,3 +48,16 @@ const startImage = () =>{
 setInterval(startImage,1000);
 
 /* sm image slider end */
+
+const spyEls = document.querySelectorAll('.scroll-spy')
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+/*메뉴 클릭 시 색 변화 유지 */
